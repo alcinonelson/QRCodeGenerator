228dc7f9e143b03f0a429061b1422a6188e95738
Alcino <alcino.maido99@gmail.com>
2025-08-26 20:53:19 +0200

    Use dotenv and hide ports in .env for backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import QRCode from 'qrcode';
 import cors from 'cors';
+import "dotenv/config";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.get('/', async (req, res) => {
   res.send({
